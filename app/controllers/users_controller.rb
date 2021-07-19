3bea7e74254bc83c8948c183d4ef5986ae5aeceb
Peter Abah <peterabah.ob@gmail.com>
2021-07-19 22:45:24 +0100

    add #create method in UsersControllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(username: params[:username], password: params[:password], email: params[:email])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 end
